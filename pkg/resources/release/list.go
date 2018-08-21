e5f0f314ae5d5defe26341b3b6af61935800d885
Zexi Li <lizexi@yunionyun.com>
2018-08-21 20:05:06 +0800

    vendor update

--- pkg/resources/release/list.go
+++ pkg/resources/release/list.go
@@ -45,11 +45,76 @@ func (man *SReleaseManager) List(req *common.Request) (common.ListResource, erro
 		return nil, err
 	}
 	defer cli.Close()
-	return man.GetReleaseList(cli, req.GetNamespaceQuery(), req.ToQuery())
+	q := ReleaseListQuery{}
+	err = req.Query.Unmarshal(&q)
+	if err != nil {
+		return nil, err
+	}
+	return man.GetReleaseList(cli, q, req.ToQuery())
 }
 
-func (man *SReleaseManager) GetReleaseList(helmclient *client.HelmTunnelClient, nsQuery *common.NamespaceQuery, dsQuery *dataselect.DataSelectQuery) (*ReleaseList, error) {
-	list, err := ListReleases(helmclient)
+type ReleaseListQuery struct {
+	Filter     string `json:"filter"`
+	All        bool   `json:"all"`
+	Namespace  string `json:"namespace"`
+	Admin      bool   `json:"admin"`
+	Deployed   bool   `json:"deployed"`
+	Deleted    bool   `json:"deleted"`
+	Deleting   bool   `json:"deleting"`
+	Failed     bool   `json:"failed"`
+	Superseded bool   `json:"superseded"`
+	Pending    bool   `json:"pending"`
+}
+
+func (q ReleaseListQuery) statusCodes() []release.Status_Code {
+	if q.All {
+		return []release.Status_Code{
+			release.Status_UNKNOWN,
+			release.Status_DEPLOYED,
+			release.Status_DELETED,
+			release.Status_DELETING,
+			release.Status_FAILED,
+			release.Status_PENDING_INSTALL,
+			release.Status_PENDING_UPGRADE,
+			release.Status_PENDING_ROLLBACK,
+		}
+	}
+
+	status := []release.Status_Code{}
+	if q.Deployed {
+		status = append(status, release.Status_DEPLOYED)
+	}
+
+	if q.Deleted {
+		status = append(status, release.Status_DELETED)
+	}
+
+	if q.Deleting {
+		status = append(status, release.Status_DELETING)
+	}
+
+	if q.Failed {
+		status = append(status, release.Status_FAILED)
+	}
+
+	if q.Superseded {
+		status = append(status, release.Status_SUPERSEDED)
+	}
+
+	if q.Pending {
+		status = append(status, release.Status_PENDING_INSTALL, release.Status_PENDING_UPGRADE, release.Status_PENDING_UPGRADE)
+	}
+
+	if len(status) == 0 {
+		// Default case
+		status = append(status, release.Status_DEPLOYED, release.Status_FAILED, release.Status_PENDING_INSTALL)
+	}
+
+	return status
+}
+
+func (man *SReleaseManager) GetReleaseList(helmclient *client.HelmTunnelClient, q ReleaseListQuery, dsQuery *dataselect.DataSelectQuery) (*ReleaseList, error) {
+	list, err := ListReleases(helmclient, q)
 	if err != nil {
 		return nil, err
 	}
@@ -82,17 +147,22 @@ func ToReleaseList(releases []*release.Release, dsQuery *dataselect.DataSelectQu
 	return list, err
 }
 
-func ListReleases(helmclient *client.HelmTunnelClient) (*rls.ListReleasesResponse, error) {
-	stats := []release.Status_Code{
-		release.Status_DEPLOYED,
-	}
-	resp, err := helmclient.ListReleases(
-		helm.ReleaseListFilter(""),
+func ListReleases(helmclient *client.HelmTunnelClient, q ReleaseListQuery) (*rls.ListReleasesResponse, error) {
+	stats := q.statusCodes()
+	ops := []helm.ReleaseListOption{
 		helm.ReleaseListSort(int32(rls.ListSort_LAST_RELEASED)),
 		helm.ReleaseListOrder(int32(rls.ListSort_DESC)),
 		helm.ReleaseListStatuses(stats),
-	)
+	}
+	if len(q.Filter) != 0 {
+		log.Debugf("Apply filters: %v", q.Filter)
+		ops = append(ops, helm.ReleaseListFilter(q.Filter))
+	}
+	if len(q.Namespace) != 0 {
+		ops = append(ops, helm.ReleaseListNamespace(q.Namespace))
+	}
 
+	resp, err := helmclient.ListReleases(ops...)
 	if err != nil {
 		log.Errorf("Can't retrieve the list of releases: %v", err)
 		return nil, err
