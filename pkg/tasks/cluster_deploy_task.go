9995f8a3c2fd65f4277da1dca149d050c7cc485d
Zexi Li <lizexi@yunionyun.com>
2018-10-09 17:31:36 +0800

    add/remove schedtag when node added or deleted

--- pkg/tasks/cluster_deploy_task.go
+++ pkg/tasks/cluster_deploy_task.go
@@ -3,15 +3,21 @@ package tasks
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"yunion.io/x/jsonutils"
 	"yunion.io/x/log"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db"
 	"yunion.io/x/onecloud/pkg/cloudcommon/db/taskman"
+	cloudmod "yunion.io/x/onecloud/pkg/mcclient/modules"
 
 	"yunion.io/x/yunion-kube/pkg/controllers"
 	"yunion.io/x/yunion-kube/pkg/models"
+)
+
+const (
+	CONTAINER_SCHED_TAG = "container"
 )
 
 type ClusterDeployTask struct {
@@ -73,11 +79,16 @@ func (t *ClusterDeployTask) OnWaitNodesAgentStartFailed(ctx context.Context, obj
 
 func (t *ClusterDeployTask) doDeploy(ctx context.Context, cluster *models.SCluster, nodes []*models.SNode) {
 	postDeployFunc := func() {
-		err := controllers.Manager.AddController(cluster)
+		err := addNodesToContainerSchedtag(nodes)
+		if err != nil {
+			log.Errorf("Add node to container schedtag error: %v", err)
+		}
+		err = controllers.Manager.AddController(cluster)
 		if err != nil {
 			log.Errorf("Start controller of cluster %q error: %v", cluster.Name, err)
 		}
 	}
+
 	err := cluster.Deploy(ctx, postDeployFunc, nodes...)
 	if err != nil {
 		log.Errorf("Deploy error: %v", err)
@@ -86,4 +97,45 @@ func (t *ClusterDeployTask) doDeploy(ctx context.Context, cluster *models.SClust
 	}
 
 	t.SetStageComplete(ctx, nil)
+}
+
+func createContainerSchedtag() error {
+	params := jsonutils.NewDict()
+	params.Add(jsonutils.NewString(CONTAINER_SCHED_TAG), "name")
+	params.Add(jsonutils.NewString("Allow run container"), "description")
+	s, err := models.GetAdminSession()
+	if err != nil {
+		return err
+	}
+	_, err = cloudmod.Schedtags.Create(s, params)
+	if err != nil {
+		if strings.Contains(err.Error(), "Duplicate name") {
+			return nil
+		}
+		return err
+	}
+	return nil
+}
+
+func addNodeToContainerSchedtag(node *models.SNode) error {
+	s, err := models.GetAdminSession()
+	if err != nil {
+		return err
+	}
+	_, err = cloudmod.Schedtaghosts.Attach(s, CONTAINER_SCHED_TAG, node.Name, nil)
+	return err
+}
+
+func addNodesToContainerSchedtag(nodes []*models.SNode) error {
+	err := createContainerSchedtag()
+	if err != nil {
+		return err
+	}
+	for _, node := range nodes {
+		err = addNodeToContainerSchedtag(node)
+		if err != nil {
+			log.Errorf("Add node %s to container schedtag error: %v", node.Name, err)
+		}
+	}
+	return nil
 }
