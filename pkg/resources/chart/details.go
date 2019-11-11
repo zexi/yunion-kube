6e75177eda8af002789fbcab05b88065882cd009
Zexi Li <zexi.li@qq.com>
2019-11-11 17:36:11 +0800

    chart: details add files

--- pkg/resources/chart/details.go
+++ pkg/resources/chart/details.go
@@ -1,7 +1,11 @@
 package chart
 
 import (
+	"path/filepath"
+
 	"encoding/base64"
+
+	"helm.sh/helm/v3/pkg/chart"
 
 	"yunion.io/x/yunion-kube/pkg/apis"
 	"yunion.io/x/yunion-kube/pkg/helm"
@@ -13,19 +17,27 @@ func (man *SChartManager) Show(repoName, chartName, version string) (interface{}
 }
 
 func (man *SChartManager) GetDetails(repoName, chartName, version string) (*apis.ChartDetail, error) {
-	chart, err := helm.NewChartClient(options.Options.HelmDataDir).Show(repoName, chartName, version)
+	chObj, err := helm.NewChartClient(options.Options.HelmDataDir).Show(repoName, chartName, version)
 	if err != nil {
 		return nil, err
 	}
 	readmeStr := ""
-	readmeFile := helm.FindChartReadme(chart)
+	readmeFile := helm.FindChartReadme(chObj)
 	if readmeFile != nil {
 		readmeStr = base64.StdEncoding.EncodeToString(readmeFile.Data)
 	}
+	files := make([]*chart.File, len(chObj.Raw))
+	for idx, rf := range chObj.Raw {
+		files[idx] = &chart.File{
+			Name: filepath.Join(chObj.Name(), rf.Name),
+			Data: rf.Data,
+		}
+	}
 	return &apis.ChartDetail{
 		Repo:   repoName,
-		Name:   chart.Name(),
-		Chart:  chart,
+		Name:   chObj.Name(),
+		Chart:  chObj,
 		Readme: readmeStr,
+		Files:  files,
 	}, nil
 }
